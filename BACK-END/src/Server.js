6e6fd36a8e15a76b564ea6168b2347cfbbd04a70
Muhammed siyad <siyadkundala10@gmail.com>
2024-09-12 10:06:45 +0530

    ss

--- BACK-END/src/Server.js
+++ BACK-END/src/Server.js
@@ -1,36 +1,37 @@
 // import express from 'express' 
 require('dotenv').config() 
 const express = require("express"); 
-const app = express();     
+const app = express();      
 const cors = require("cors");      
 const morgan = require("morgan");    
-const database = require('../src/models/connection')   
+const database = require('../src/models/connection')    
   
-const userRouter = require("./Router/userRouter");   
-const adminRouter = require("./Router/adminRouter");     
-     
-database()     
-       
-app.use(express.json());      
-app.use(express.urlencoded({ extended: true }));  
-
-app.use(             
+const userRouter = require("./Router/userRouter");    
+const adminRouter = require("./Router/adminRouter");      
+      
+database()      
+         
+app.use(express.json());        
+app.use(express.urlencoded({ extended: true }));   
+ 
+app.use(              
   cors({     
     origin: "http://localhost:5173",            
-    credentials: true,
+    credentials: true, 
   })   
 );   
-
+ 
 
 app.use(morgan("tiny"));         
-    
+     
 app.get("/", (req, res) => { 
-  res.send("Server ok");         
-});
-       
-app.use("/user", userRouter);
-app.use("/admin", adminRouter);         
-
+  res.send("Server ok");          
+}); 
+        
+app.use("/user", userRouter);   
+app.use("/admin", adminRouter);           
+ 
 app.listen(3000, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);  
+  console.log(`Server is running on port ${process.env.PORT}`);   
 });
+ 
