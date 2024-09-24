d4ec1214823fb894ca2bc511f16e1deae89667c2
Muhammed siyad <siyadkundala10@gmail.com>
2024-09-24 09:29:44 +0530

    commited

--- BACK-END/src/Server.js
+++ BACK-END/src/Server.js
@@ -15,23 +15,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));   
  
 app.use(              
-  cors({     
+  cors({      
     origin: "http://localhost:5173",            
     credentials: true, 
-  })   
+  })    
 );   
- 
+    
    
-app.use(morgan("tiny"));         
+app.use(morgan("tiny"));           
      
 app.get("/", (req, res) => {         
-  res.send("Server ok");          
+  res.send("Server ok");            
 });   
-        
+         
 app.use("/user", userRouter);   
 app.use("/admin", adminRouter);                    
-     
-app.listen(3000, () => {
+      
+app.listen(3000, () => { 
   console.log(`Server is running on port ${process.env.PORT}`);   
 });    
        
