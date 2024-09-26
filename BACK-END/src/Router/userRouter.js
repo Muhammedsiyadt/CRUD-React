74d598178169c647beda6975d6c57b28d2cd1c83
Muhammed siyad <siyadkundala10@gmail.com>
2024-09-26 14:40:04 +0530

    ss

--- BACK-END/src/Router/userRouter.js
+++ BACK-END/src/Router/userRouter.js
@@ -4,11 +4,11 @@ const { routeProtect, isUser } = require('../Middleware/Middleware');
 const upload = require('../cloudinary'); // Path to your Multer configuration
 const userController = require('../controller/userController/userController');
 
-router.post('/login', userController.login);
-router.post('/signup', userController.signUp);
+router.post('/login', userController.login);  
+router.post('/signup', userController.signUp); 
 router.get('/get/userData', routeProtect, isUser , userController.getUserData);
 router.put('/updateprofile', routeProtect,isUser, upload.single('image'), userController.updateProfile);
 router.get('/userHome/userData', routeProtect,isUser, userController.getDataFromHome)
  
-module.exports = router; 
+module.exports = router;  
    
--- BACK-END/src/cloudinary.js
+++ BACK-END/src/cloudinary.js
@@ -4,12 +4,12 @@ const cloudinary = require('./cloudinaryConfig');
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
-    folder: 'profile_pictures',
+  params: { 
+    folder: 'profile_pictures', 
     allowedFormats: ['jpg', 'jpeg', 'png'],
-  },
+  },  
 });    
 
-const upload = multer({ storage });
+const upload = multer({ storage });  
 
 module.exports = upload;
